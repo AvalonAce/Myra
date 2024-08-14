3196d078de7f4f75dd6ae5d1ade2c72f50abb77e
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2024-08-14 09:56:26 -0400

    fixed untracked files

--- audio/Response_Variation.py
+++
@@ -1,4 +0,0 @@
-# class Assistant_Variation:
-
-# To do: Implement the Response_Variation class where the variation of the response is calculated based on the query
-    
--- response/CONVERSE.py
+++
@@ -1,1 +0,0 @@
-# TO DO: Implement this mode as a conversation mode where the assistant can engage in a conversation with the user, similar to ASSISTANT mode but with more advanced responses and more engaging conversation.
